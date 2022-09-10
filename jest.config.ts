5a3de83aabe6947721e20c651c5f9f0b0bf8f884
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2022-09-10 19:21:15 -0300

    fix: jest error syntax

--- jest.config.ts
+++ jest.config.ts
@@ -35,7 +35,24 @@ export default {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
 
+  testPathIgnorePatterns: ['/node_modules/'],
+  modulePaths: ['<rootDir>/src'],
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.tsx', '!src/**/*.spec.tsx', '!src/**/*App.tsx', '!src/index.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.tsx',
+    '!src/**/*.spec.tsx',
+    '!src/**/*App.tsx',
+    '!src/index.tsx',
+    '!src/main.tsx',
+    '!src/routers/index.tsx',
+    '!src/layouts/DefaultLayout.tsx',
+  ],
   coverageReporters: ['lcov', 'json'],
 }
--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -5,7 +5,9 @@ import { Home } from '../../pages/Home'
 describe('Home', () => {
   it('renders a hello world', () => {
     render(<Home />)
+    // eslint-disable-next-line testing-library/no-debugging-utils
+    screen.logTestingPlaygroundURL()
 
-    screen.getByRole('heading', { name: /Hello World/i })
+    // screen.getByRole('heading', { name: /Hello World/i })
   })
 })
