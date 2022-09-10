29cf3ea73b39ef0e605572e63cab25b101106404
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2022-09-10 19:28:50 -0300

    test: Home test

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -5,9 +5,11 @@ import { Home } from '../../pages/Home'
 describe('Home', () => {
   it('renders a hello world', () => {
     render(<Home />)
-    // eslint-disable-next-line testing-library/no-debugging-utils
-    screen.logTestingPlaygroundURL()
-
-    // screen.getByRole('heading', { name: /Hello World/i })
+    screen.getByRole('heading', { name: /encontre o café perfeito para qualquer hora do dia/i })
+    screen.getByText(/com o coffee delivery você recebe seu café onde estiver, a qualquer hora/i)
+    screen.getByText(/compra simples e segura/i)
+    screen.getByText(/embalagem mantém o café intacto/i)
+    screen.getByText(/embalagem mantém o café intacto/i)
+    screen.getByText(/o café chega fresquinho até você/i)
   })
 })
